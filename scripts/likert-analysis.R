b905a1fc5ee6d63d81d16fbc74d8e01706eaa4ca
JP Flores <jpflores@jps-macbook-pro.wireless-1x.unc.edu>
2024-05-21 11:22:00 -0400

    Add statistical tests

--- scripts/likert-analysis.R
+++ scripts/likert-analysis.R
@@ -11,12 +11,14 @@ res <- readxl::read_xlsx("data/PIERE+Post-Assessment_April+1,+2024_06.58.xlsx",
   select(-c(19:20)) |>
   na.omit()
 
-levels <- c("Not knowledgeable at all", "Slightly knowledgeable", "Moderately knowledgeable",
+levels_knowledge <- c("Not knowledgeable at all", "Slightly knowledgeable", "Moderately knowledgeable",
             "Very knowledgeable", "Extremely knowledgeable")
+
+labels <- 1:5
 
 ## Add levels to each factor column in res_knowledge
 res_knowledge <- res |> 
-  mutate(across(everything(), factor, levels = levels))
+  mutate(across(everything(), factor, levels = levels_knowledge))
 
 # Likert Scale for Knowledge ----------------------------------------------
 ## Knowledge of people's identities influence their experience in higher education and STEM
@@ -63,18 +65,36 @@ summary(likert_knowledge)
 plot(likert_knowledge, legend.position = "right")
 
 ## Check for Statistical significance 
+stats_knowledge <- res |> 
+  mutate(across(everything(), ~ factor(.x, levels = levels_knowledge, labels = labels))) |> 
+  mutate(across(everything(), as.numeric)) 
 
+## Knowledge of the impact of biases, discrimination, and microaggressions on individuals, organizations, and society
+wilcox.test(stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_the_impact_of_biases_discrimination_and_microaggressions_on_individuals_organizations_and_society_before_this_class,
+            stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_the_impact_of_biases_discrimination_and_microaggressions_on_individuals_organizations_and_society_after_this_class,
+            paired = TRUE)
+
+## Knowledge of how peoples' identities influence their experience in higher education and STEM 
+wilcox.test(stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_how_peoples_identities_influence_their_experience_in_higher_education_and_stem_before_this_class,
+            stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_how_peoples_identities_influence_their_experience_in_higher_education_and_stem_after_this_class,
+            paired = TRUE)
+
+## Knowledge of barriers that exclude people with marginalized identities from higher education and STEM
+wilcox.test(stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_barriers_that_exclude_people_with_marginalized_identities_from_higher_education_and_stem_before_this_class,
+            stats_knowledge$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_knowledge_of_barriers_that_exclude_people_with_marginalized_identities_from_higher_education_and_stem_after_this_class,
+            paired = TRUE)
 
 # Likert Scale for Ability ------------------------------------------------
 
 res_ability <- res |> 
   select(c(7:18))
 
-levels <- c("None", "Low", "Average",
+levels_ability <- c("None", "Low", "Average",
             "Moderately high", "Very high")
 
+## Add levels to each factor column in res_knowledge
 res_ability <- res_ability |> 
-  mutate(across(everything(), factor, levels = levels))
+  mutate(across(everything(), factor, levels = levels_ability))
 
 ## Ability to use inclusive language (ex. partner, unhoused) in different personal and professional contexts
 res_ability_language_before <- res_ability |> 
@@ -152,3 +172,39 @@ likert_ability <- likert(res_ability[,c(1, 3:7)], grouping = res_ability$interve
 summary(likert_ability)
 
 plot(likert_ability, legend.position = "right")
+
+## stats
+stats_ability <- res |>
+  mutate(across(everything(), ~ factor(.x, levels = levels_ability, labels = labels))) |>
+  mutate(across(everything(), as.numeric)) |> 
+  select(-c(1:6))
+
+## Ability to
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_use_inclusive_language_ex_partner_unhoused_in_different_personal_and_professional_contexts_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_use_inclusive_language_ex_partner_unhoused_in_different_personal_and_professional_contexts_after_this_class,
+            paired = TRUE)
+
+## Ability to
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_seek_out_and_embracing_unique_perspectives_of_those_with_different_backgrounds_and_respect_their_lived_experiences_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_seek_out_and_embracing_unique_perspectives_of_those_with_different_backgrounds_and_respect_their_lived_experiences_after_this_class,
+            paired = TRUE)
+
+## Ability to 
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_adopt_an_open_mindset_of_continuous_learning_to_promote_an_environment_where_we_can_and_do_learn_from_our_mistakes_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_adopt_an_open_mindset_of_continuous_learning_to_promote_an_environment_where_we_can_and_do_learn_from_our_mistakes_after_this_class,
+            paired = TRUE)
+
+## Ability to 
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_speak_out_against_harmful_language_whether_or_not_an_individual_from_the_marginalized_group_is_present_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_speak_out_against_harmful_language_whether_or_not_an_individual_from_the_marginalized_group_is_present_after_this_class,
+            paired = TRUE)
+
+## Ability to 
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_advocate_for_opportunities_for_individuals_with_marginalized_identities_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_advocate_for_opportunities_for_individuals_with_marginalized_identities_after_this_class,
+            paired = TRUE)
+
+## Ability to 
+wilcox.test(stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_facilitate_an_environment_where_everyone_has_a_voice_at_the_table_before_this_class,
+            stats_ability$thinking_about_yourself_now_and_before_this_class_how_do_you_rate_your_ability_to_facilitate_an_environment_where_everyone_has_a_voice_at_the_table_after_this_class,
+            paired = TRUE)
